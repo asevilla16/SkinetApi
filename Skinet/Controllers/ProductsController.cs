8a1d37d16ff7da1933cf76477771a0c89828777c
asevilla16 <angelsevilla1997@gmail.com>
2023-11-23 11:16:38 -0600

    End of section 2

---
+++ Skinet/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Skinet.Infrastructure.Data;
+using Skinet.Core.Entities;
+
+namespace Skinet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Product>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Product> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
